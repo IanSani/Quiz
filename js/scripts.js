2de2867a701a60a552f868897c4c1e939e959892
IanSani <iansani259@gmail.com>
2019-02-25 10:26:51 +0300

    Added if statements

--- js/scripts.js
+++ js/scripts.js
@@ -7,6 +7,17 @@ $(document).ready(function(){
     var number3=parseInt($("input:radio[name=number3]:checked").val());
     var number4=parseInt($("input:radio[name=number4]:checked").val());
     var total=(number1+number2+number3+number4);
+    var percentage=((total/4)*100);
+    $("#score").text("You have scored:"+percentage + "%");
+    if(percentage>80){
+      $("#comment").text("Nice trial,passed")
+    }
+    else if((percentage>50)&&(percentage<79)){
+      $("#comment").text("Average,keep up.");
+    }
+    else{
+      $("#comment").text("Need to study more,fail.");
+    }
 
   });
 });
