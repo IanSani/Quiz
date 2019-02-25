998a7a4e365304c45fbcb0545198a8992fa873fe
IanSani <iansani259@gmail.com>
2019-02-25 10:06:07 +0300

    Added function to js file

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+// This is the user interface logic:
+
+function check(){
+    var number1=document.quiz.choice.value;
+    var number2=document.quiz.choice2.value;
+    var number3=document.quiz.choice3.value;
+    var mumber4=document.quiz.choice4.value;
+    var correct=25;
+
+    if(choice == "Scripting"){
+      correct
+    }
+
+
+  document.getElementById("after submit").style.visibility="visible";
+  document.getElementById("number_correct").innerHTML="You got " +correct + " correct.";
+};
