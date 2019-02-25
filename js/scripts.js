81ae7120844e0d13b228ba8d5bd5623fae1f6147
IanSani <iansani259@gmail.com>
2019-02-25 10:33:36 +0300

    Added another div id statement to index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,20 @@
 // This is the user interface logic:
-$(document).ready(function(){
-  $("form#quiz").submit(function(event){
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
     event.preventDefault();
-    var number1=parseInt($("input:radio[name=number1]:checked").val());
-    var number2=parseInt($("input:radio[name=number2]:checked").val());
-    var number3=parseInt($("input:radio[name=number3]:checked").val());
-    var number4=parseInt($("input:radio[name=number4]:checked").val());
-    var total=(number1+number2+number3+number4);
-    var percentage=((total/4)*100);
-    $("#score").text("You have scored:"+percentage + "%");
-    if(percentage>80){
-      $("#comment").text("Nice trial,passed")
-    }
-    else if((percentage>50)&&(percentage<79)){
-      $("#comment").text("Average,keep up.");
-    }
-    else{
-      $("#comment").text("Need to study more,fail.");
+    var number1 = parseInt($("input:radio[name=number1]:checked").val());
+    var number2 = parseInt($("input:radio[name=number2]:checked").val());
+    var number3 = parseInt($("input:radio[name=number3]:checked").val());
+    var number4 = parseInt($("input:radio[name=number4]:checked").val());
+    var total = (number1 + number2 + number3 + number4);
+    var percentage = ((total / 4) * 100);
+    $("#result").text("You have scored:" + percentage + "%");
+    if (percentage > 80) {
+      $("#statement").text("Nice trial,passed")
+    } else if ((percentage > 50) && (percentage < 79)) {
+      $("#statement").text("Average,keep up.");
+    } else {
+      $("#statement").text("Need to study more,fail.");
     }
 
   });
